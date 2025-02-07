e49ee2a4a8ce3718a8791c0bf103dd2a2bf92e6f
abdul <abdulwabby@gmail.com>
2025-02-07 23:12:03 +0500

    final changes

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -23,23 +23,23 @@ export default function HeroSection() {
 
       {/* Content */}
       <div className="relative z-10 flex flex-col items-center justify-center min-h-[800px] px-4 text-center ">
-        <h1 className="max-w-2xl mb-2 text-4xl md:text-5xl font-bold text-white font-saira bg-gradient-to-b from-[#E1BBFB] to-white bg-clip-text text-transparent">
+        <h1 className="max-w-2xl mb-2 text-4xl md:text-5xl font-bold font-saira bg-gradient-to-b from-[#E1BBFB] to-white bg-clip-text text-transparent">
           Collect your XP points.
         </h1>
-        <h2 className="max-w-2xl text-4xl md:text-5xl font-bold text-white font-saira bg-gradient-to-b from-[#E1BBFB] to-white bg-clip-text text-transparent">
+        <h2 className="max-w-2xl text-4xl md:text-5xl font-bold  font-saira bg-gradient-to-b from-[#E1BBFB] to-white bg-clip-text text-transparent">
           Register for free and
         </h2>
-        <h2 className="max-w-2xl text-4xl md:text-5xl font-bold text-white font-saira bg-gradient-to-b from-[#E1BBFB] to-white bg-clip-text text-transparent">
+        <h2 className="max-w-2xl text-4xl md:text-5xl font-bold font-saira bg-gradient-to-b from-[#E1BBFB] to-white bg-clip-text text-transparent">
           earn rewards!
         </h2>
-        <p className="my-8 text-sm text-gray-300 max-w-xs font-sarif font-thin">
+        <p className="my-8 text-sm text-gray-300 max-w-xs font-sarif font-normal">
           Creative design to build a brand that's strong, impactful, and full of
           meaning
         </p>
         <div className="flex flex-col sm:flex-row gap-4">
           <button
             className="px-8 py-3 bg-white text-black rounded-xl hover:bg-gray-100 transition-colors duration-300 "
-            style={{ boxShadow: "inset 0 -4px 6px rgba(0, 0, 0, 0.2)" }}
+            style={{ boxShadow: "inset 0 -10px 10px rgba(0, 0, 0, 0.2)" }}
           >
             Register For Free
           </button>
